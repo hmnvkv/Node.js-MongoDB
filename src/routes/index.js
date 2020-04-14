f437cd50d8162d9f528174e05439a8f4428a978b
hmnvkv <manrique.hamed1610@gmail.com>
2020-04-14 17:11:23 -0500

    Simple changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const image = require('../controllers/image');
 module.exports = app => {
     router.get('/', home.index);
     router.get('/images/:image_id', image.index);
-    router.post('/', image.create);
+    router.post('/images', image.create);
     router.post('/images/:image:id/like', image.like);
     router.post('/images/:image:id/comments', image.comment);
     router.delete('/images/:image:id', image.delete);
