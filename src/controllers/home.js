5b72c798826ce25fd35f33378b017c1b66ea0079
hmnvkv <manrique.hamed1610@gmail.com>
2020-04-14 15:47:50 -0500

    Routes

---
+++ src/controllers/home.js
@@ -0,0 +1,9 @@
+const ctrl = {};
+
+ctrl.index = (req, res) => {
+    res.send('Index');
+};
+
+
+
+module.exports = ctrl; 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,18 @@
+const express = require('express');
+const router = express.Router();
+/* Controllers */
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+
+/* Rutas */
 module.exports = app => {
-    app.get('/', (req, res) => {
-        res.send('Index');
-    });
+    router.get('/', home.index);
+    router.get('/images/:image_id', image.index);
+    router.post('/', image.create);
+    router.post('/images/:image:id/like', image.like);
+    router.post('/images/:image:id/comments', image.comment);
+    router.delete('/images/:image:id', image.delete);
+
+    app.use(router);
+
 };
